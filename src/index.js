cfe14405afb70c7ee496a9c7321282babb0106d4
Angey22 <babandval@gmail.com>
2023-06-29 17:53:16 +0300

    Added first component & settings for import wayvse

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
+// Импорт "глобальных" элементов.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Импорт "локальных" элементов.
+import './index.css';
+import { App } from 'components/App';
+
+// Упрощенная запись начальной точки "рендера".
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
+
+// Стандартная запись начальной точки "рендера".
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+//! Код написанный в файле "jsconfig.json" позволяет устанавливать все пути для импорта файлов относительно директории "src", благодаря чему упрощается адресация при настройке путей импорта функций (компонентов) из одного файла в другой. 
 
